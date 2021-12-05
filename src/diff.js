2a46209e0df858a22e1fe3aaf8b243268b0b0024
Arthur Arzhannikov <arzartden@yandex.ru>
2021-12-05 23:47:38 +0700

    add src

---
+++ src/diff.js
@@ -0,0 +1,11 @@
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+program
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format');
+
+export default program;
