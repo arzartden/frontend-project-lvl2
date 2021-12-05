281117d60607848c74fc73f949c58974fce79058
Arthur Arzhannikov <arzartden@yandex.ru>
2021-12-05 18:07:58 +0700

    completion of the second step of the project

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -4,9 +4,12 @@ import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
 
