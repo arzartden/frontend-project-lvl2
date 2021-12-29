226760bc4056738941e58172054cfe6d0a3f26dc
Arthur Arzhannikov <arzartden@yandex.ru>
2021-12-29 23:54:45 +0700

    edit parser.js

--- src/parsers.js
+++ src/parsers.js
@@ -8,13 +8,15 @@ const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 const getParse = (filename) => {
   const format = path.extname(getFixturePath(filename));
   const data = readFile(filename);
-  let parse;
-  if (format === '.json') {
-    parse = JSON.parse;
-  } else if (format === '.yml' || format === '.yaml') {
-    parse = yaml.load;
+  switch (format) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+    case '.yaml':
+      return yaml.load(data);
+    default:
+      throw new Error('Unsupported content format. Use json or yml, yaml');
   }
-  return parse(data);
 };
 
 export default getParse;
